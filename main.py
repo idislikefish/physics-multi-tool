6be6b5c1b93dff2eaa202bc73c0f4edd59d4cb58
Garry <github@garrynet.co.uk>
2024-03-13 16:27:06 +0000

    added topic
    - main.py: added ability to browse topics
    - script.js removed placeholder function and added navigation as well as
      first calculator
    - style.css added selector style
    - added templates

--- main.py
+++ main.py
@@ -6,5 +6,12 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/topics/<page>")
+def calc(page):
+    if page == "add":
+        return render_template("add-topic.html")
+    return render_template(page+".html")
+
+
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,3 +1,17 @@
-function hello_world() { // Placeholder function
-    alert("Hello World!");
+function move_to_topic() {
+    var selection = document.getElementById("select").value;
+
+    document.location.href = "/topics/" + selection
+}
+
+function newton2() {
+    var mass = document.getElementById("mass").value;
+    var acc = document.getElementById("acc").value;
+    var answer = document.getElementById("answer");
+
+    if (mass && acc) {
+        answer.innerText = "F = " + mass * acc;
+    } else {
+        answer.innerText = "F ="
+    }
 }
